b10f755b2de65430554e8535dec3e097d740d1fc
Boyan Todorov <r0b0engi2T@gmail.com>
2022-10-20 08:56:25 +0200

    Update
    Added save file location changed style

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from os import path
 from PyQt6.uic import loadUiType
 FORM_CLASS,_ = loadUiType(path.join(path.dirname('__file__'), 'main.ui'))
 from report import Report
+from datetime import datetime
+
 
 class Main(QMainWindow, FORM_CLASS):
     def __init__(self, parent= None):
@@ -18,13 +20,14 @@ class Main(QMainWindow, FORM_CLASS):
         self.btn_post_3.clicked.connect(self.jts1_report)
     def mki_report(self):
         r = Report()
-        r.agregate_storage_type(r.mki, 15, "MKI") 
+        r.agregate_storage_type(r.mki, 15, "MKI", "S:\Warehouse dept\Inventory control\Reporting and dashboards\Cycle_Count\Reports_CycleCount\MKI\\"+"MKI_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + '.pdf') 
+        print("Report Generated")
     def jts1_report(self):
         r = Report()
-        r.agregate_storage_type(r.jts1, 10, "JTS1")
+        r.agregate_storage_type(r.jts1, 10, "JTS1", "S:\Warehouse dept\Inventory control\Reporting and dashboards\Cycle_Count\Reports_CycleCount\JTS1\\"+"JTS1_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + '.pdf')
     def jts2_report(self):
         r = Report()
-        r.agregate_storage_type(r.jts2, 10, "JTS2")
+        r.agregate_storage_type(r.jts2, 10, "JTS2", "S:\Warehouse dept\Inventory control\Reporting and dashboards\Cycle_Count\Reports_CycleCount\JTS2\\"+"JTS2_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + '.pdf')
 
 def main():
     app = QApplication(sys.argv)
