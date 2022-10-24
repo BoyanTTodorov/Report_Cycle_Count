f2af0df38bbe09943a0c5aa6085156d659c3f80f
Boyan Todorov <r0b0engi2T@gmail.com>
2022-10-24 09:15:57 +0200

    Roll back to Pyqt5
    Roll back to PyQt5 to solve the back-end issue. with not showing a chart window.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from PyQt6.QtWidgets import *
-from PyQt6.QtGui import *
-from PyQt6.QtCore import *
-from PyQt6.QtWidgets import QMainWindow,QApplication
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QMainWindow,QApplication
 import sys
 from os import path
-from PyQt6.uic import loadUiType
+from PyQt5.uic import loadUiType
 FORM_CLASS,_ = loadUiType(path.join(path.dirname('__file__'), 'main.ui'))
 from report import Report
 from datetime import datetime
+import os
 
 
 class Main(QMainWindow, FORM_CLASS):
@@ -18,27 +19,28 @@ class Main(QMainWindow, FORM_CLASS):
         self.btn_post.clicked.connect(self.mki_report)
         self.btn_post_2.clicked.connect(self.jts2_report)
         self.btn_post_3.clicked.connect(self.jts1_report)
+        self.user = os.getlogin()
     def mki_report(self):
         r = Report()
-        r.agregate_storage_type(r.mki, 15, "MKI", "S:\Warehouse dept\Inventory control\Reporting and dashboards\Cycle_Count\Reports_CycleCount\MKI\\"+"MKI_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + '.pdf') 
-        print(r.mki.describe)
-        print("Report Generated")
+        r.agregate_storage_type(r.mki, 15, "MKI", "S:\Warehouse dept\Inventory control\Reporting and dashboards\Cycle_Count\Reports_CycleCount\MKI\\"+"MKI_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + f"_{self.user.upper()}"+'.pdf') 
+        #print(r.mki.describe)
+        #print("Report Generated")
     def jts1_report(self):
         r = Report()
-        r.agregate_storage_type(r.jts1, 10, "JTS1", "S:\Warehouse dept\Inventory control\Reporting and dashboards\Cycle_Count\Reports_CycleCount\JTS1\\"+"JTS1_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + '.pdf')
-        print(r.jts1.describe)
-        print("Report Generated")
+        r.agregate_storage_type(r.jts1, 10, "JTS1", "S:\Warehouse dept\Inventory control\Reporting and dashboards\Cycle_Count\Reports_CycleCount\JTS1\\"+"JTS1_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + f"_{self.user.upper()}"+'.pdf')
+        #print(r.jts1.describe)
+        #print("Report Generated")
     def jts2_report(self):
         r = Report()
-        print(r.jts2.describe)
-        print("Report Generated")
-        r.agregate_storage_type(r.jts2, 10, "JTS2", "S:\Warehouse dept\Inventory control\Reporting and dashboards\Cycle_Count\Reports_CycleCount\JTS2\\"+"JTS2_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + '.pdf')
+        #print(r.jts2.describe)
+        #print("Report Generated")
+        r.agregate_storage_type(r.jts2, 10, "JTS2", "S:\Warehouse dept\Inventory control\Reporting and dashboards\Cycle_Count\Reports_CycleCount\JTS2\\"+"JTS2_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + f"_{self.user.upper()}"+'.pdf')
 
 def main():
     app = QApplication(sys.argv)
     window = Main()
     window.show()
-    app.exec()
+    app.exec_()
 
 if __name__ == '__main__':
     main()
